69a9eed5793159381b2ba9ffe3de2e70037cdde4
Marian087 <rodriguezflormarian@gmail.com>
2024-04-22 20:29:02 -0300

    ej

---
+++ Ej04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Ej04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ////4. Ingresar 5 números enteros, calcular y mostrar el promedio.
+
+
+
+
+            int[] misNumeros = new int[5];
+            int suma = 0;
+            double resultado;
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ingrese el numero {i + 1} de 5 : ");
+                misNumeros[i] = int.Parse(Console.ReadLine());
+
+
+                suma += misNumeros[i];
+
+            }
+            resultado = (double)suma / misNumeros.Length;
+
+
+            Console.WriteLine($"La suma de sus números es:  {suma}");
+            Console.WriteLine($"El promedio de sus números es:  {resultado}");
+
+        }
+    }
+}
